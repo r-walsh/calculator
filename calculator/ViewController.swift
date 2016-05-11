cdf9f4550b321e50add0ee783e7c0c69f9e91aab
Ryan Walsh <ryan@thehatrack.net>
2016-05-11 15:36:30 -0500

    initial

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ViewController.swift
 //  calculator
 //
@@ -7,19 +7,125 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    var buttonSound: AVAudioPlayer!
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Empty = "Empty"
+    }
+    
+    // Initial State
+    var runningNumber = ""
+    var leftVal = ""
+    var rightVal = ""
+    var currentOperation: Operation = Operation.Empty
+    var result = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundUrl = NSURL(fileURLWithPath: path!)
+        
+        do {
+            try buttonSound = AVAudioPlayer(contentsOfURL: soundUrl)
+            buttonSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func processOperation( operation: Operation ) {
+        playSound()
+        
+        if currentOperation != Operation.Empty {
+            if runningNumber != "" {
+                rightVal = runningNumber
+                runningNumber = ""
+                
+                switch currentOperation {
+                    case Operation.Divide:
+                        result = "\(Double( leftVal )! / Double( rightVal )!)"
+                        break
+                    case Operation.Multiply:
+                        result = "\(Double( leftVal )! * Double( rightVal )!)"
+                        break
+                    case Operation.Subtract:
+                        result = "\(Double( leftVal )! - Double( rightVal )!)"
+                        break
+                    case Operation.Add:
+                        result = "\(Double( leftVal )! + Double( rightVal )!)"
+                        break
+                    default:
+                        break
+                }
+                leftVal = result
+                outputLabel.text = result
+            }
+            
+            currentOperation = operation
+            
+        } else {
+            leftVal = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
     }
-
+    
+    func playSound() {
+        if buttonSound.playing {
+            buttonSound.stop()
+        }
+        buttonSound.play()
+    }
+    
+    // MARK Actions
+    
+    @IBAction func numberPressed( button: UIButton ) {
+        playSound()
+        
+        runningNumber += "\( button.tag )"
+        outputLabel.text = runningNumber
+    }
+    
+    @IBAction func dividePressed( button: UIButton ) {
+        processOperation(Operation.Divide)
+    }
+    
+    @IBAction func multiplyPressed( button: UIButton ) {
+        processOperation(Operation.Multiply)
+    }
+    
+    @IBAction func subtractPressed( button: UIButton ) {
+        processOperation(Operation.Subtract)
+    }
+    
+    @IBAction func addPressed( button: UIButton ) {
+        processOperation(Operation.Add)
+    }
+    
+    @IBAction func equalPressed( button: UIButton ) {
+        processOperation(currentOperation)
+    }
+    
+    @IBAction func clearPressed( button: UIButton ) {
+        runningNumber = ""
+        leftVal = ""
+        rightVal = ""
+        currentOperation = Operation.Empty
+        result = ""
+        outputLabel.text = "0"
+    }
 
 }
 
